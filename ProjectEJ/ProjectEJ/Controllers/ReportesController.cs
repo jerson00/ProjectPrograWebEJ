75c93ceb030ac05f88e80514244f939b965d2b2b
jerson00 <jerson.00@hotmail.com>
2016-02-28 16:31:16 -0600

    reportes de aouestas y configuraciones

--- ProjectEJ/ProjectEJ/Controllers/ReportesController.cs
+++ ProjectEJ/ProjectEJ/Controllers/ReportesController.cs
@@ -14,16 +14,23 @@ namespace ProjectEJ.Controllers
         // GET: Reporte
         public ActionResult Index()
         {
+            var listApuestas = Apuestas.getApuestasByNum(db);
+            double montoPremios = Apuestas.getTotalPeorCaso(listApuestas);
+            
             var montoCaja = 0;
             var cajas = db.Caja.ToList();
             foreach (var caja in cajas)
             {
                 montoCaja = Convert.ToInt32(caja.Monto);
             }
-
+            double montoGananciaMinima = montoCaja - montoPremios;
+            ViewBag.MontoPremiosPeorCaso = montoPremios;
             ViewBag.MontoCaja = montoCaja;
+            ViewBag.MontoGananciaMinima = montoGananciaMinima;
             return View();
         }
+
+      
 
     }
 }
